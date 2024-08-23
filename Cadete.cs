ff18660189d89089321cde3ae8e07ac17b393e1b
NatyBlass <natypaz12345@gmail.com>
2024-08-23 19:33:20 -0300

    Constructores generados

--- Cadete.cs
+++ Cadete.cs
@@ -9,5 +9,14 @@ public class Cadete
     public int Id { get => id; set => id = value; }
     public string Nombre { get => nombre; set => nombre = value; }
     public string Telefono { get => telefono; set => telefono = value; }
-    public List<Pedidos> ListadoPedidos { get => ListadoPedidos; set => ListadoPedidos = value; }
+    public List<Pedidos> ListadoPedidos { get => listadoPedidos; set => listadoPedidos = value; }
+
+    public Cadete(int id, string nombre, string direccion, string telefono, List<Pedidos> listadoPedidos)
+    {
+        this.id = id;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.listadoPedidos = listadoPedidos;
+    }
 }
--- Cadeteria.cs
+++ Cadeteria.cs
@@ -7,4 +7,13 @@ public class Cadeteria
     public string Nombre { get => nombre; set => nombre = value; }
     public string Telefono { get => telefono; set => telefono = value; }
     public List<Cadete> ListadoCadetes1 { get => ListadoCadetes; set => ListadoCadetes = value; }
+
+
+    //Este constructor lo hice con la lupita
+    public Cadeteria(string nombre, string telefono, List<Cadete> listadoCadetes)
+    {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        ListadoCadetes = listadoCadetes;
+    }
 }
