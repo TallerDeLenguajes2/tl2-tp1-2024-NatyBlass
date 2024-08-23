f58367cfbae16a544b86525691c6373679ba6b59
NatyBlass <natypaz12345@gmail.com>
2024-08-23 18:56:30 -0300

    Metodos VerDireccion y VerDatosDelCliente realizados

--- Pedidos.cs
+++ Pedidos.cs
@@ -16,4 +16,22 @@ public class Pedidos
     public string Observacion { get => observacion; set => observacion = value; }
     public Estado EstadoPedido { get => estado; set => estado = value; }
     public DatosCliente Cliente { get => cliente; set => cliente = value; }
+
+    public void VerDireccionCliente()
+    {
+        Console.WriteLine($"La direccion del cliente {cliente.Nombre} es {cliente.Direccion}");
+    }
+
+    public void verDatosDelCliente()
+    {
+        Console.WriteLine("=========DATOS DEL CLIENTE=========");
+        Console.WriteLine($"Nombre: {cliente.Nombre}");
+        Console.WriteLine($"Direccion: {cliente.Direccion}");
+        Console.WriteLine($"Referencia: {cliente.RefDireccion}");
+        Console.WriteLine($"Telefono: {cliente.Telefono}");
+    }
+
+
+
+
 }
